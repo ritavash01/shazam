8dbc3544dcecf626f8a38018540cef49239feaf2
ritavash01 <94717544+ritavash01@users.noreply.github.com>
2024-11-27 09:44:48 +0530

    Update frb_header.cpp

--- src/shazam/sb/frb_header.cpp
+++ src/shazam/sb/frb_header.cpp
@@ -76,6 +76,7 @@ nb::dict initialize_HDR_SHM_py() {
         "C11", "C12", "C13", "C14", "E02", "E03", "E04", "E05", "E06", "S01",
         "S02", "S03", "S04", "S06", "W01", "W02", "W03", "W04", "W05", "W06"
     };
+    char beam_type[7][6]={"IA", "PA", "VLT", "PC", "CDP", "PASV","MISC"};
 
     unsigned int ref_ant_mask = 1;
     nb::list pol1_antennas;
